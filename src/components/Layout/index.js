14a279f71baa1c2e1fc9a1cd4174997ee11d4dbe
Mintu Lal Sah <mintulalsah.mailbox@gmail.com>
2024-09-13 16:17:03 +0530

    new chnage

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,8 +3,6 @@ import styles from "../../styles/employee.module.css"
 
 
 const Layout = ({ children, className }) => {
-  // Combine class names using template literals
-
   return (
     <div className={styles.mainLaout}>
       {children}
